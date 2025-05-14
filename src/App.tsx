821a0cc31491e108036c3570cbedbf12faad7ca8
aboudev22 <aboubakarjay2@gmail.com>
2025-05-14 15:57:17 +0100

    may 14

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,40 @@
+import React, { useCallback, useState } from "react";
+
+const Form = React.memo(
+  ({
+    onSubmit,
+  }: {
+    onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  }) => {
+    console.log("Form render");
+    return (
+      <form onSubmit={onSubmit}>
+        <button type="submit">Envoyer</button>
+      </form>
+    );
+  }
+);
+
 export default function App() {
-  return <div>App</div>;
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      console.log("Email:", email);
+    },
+    [email]
+  );
+
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <input
+        className="bg-violet-200 p-2 rounded-md placeholder:text-white"
+        type="email"
+        placeholder="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Form onSubmit={handleSubmit} />
+    </div>
+  );
 }
